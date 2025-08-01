4d70aa40bb9c401fdd4041a0e93aed6457216c5e
F0910054 Antonio Claudio Ferreira Filho <antonioferreira@bb.com.br>
2025-08-01 13:54:29 -0300

    Alterações realizadas durante o curso de lógica de programação com HTML e JS

--- script/main.js
+++ script/main.js
@@ -4,26 +4,54 @@
 // document.writeln(`A média das idades é ${Math.round(media)}`);
 
 // imc = peso / altura * altura;
-var nomeUsuario = validador(nomeUsuario, "Informe o seu nome:");
-var pesoUsuario = parseFloat(validador(pesoUsuario, "Informe o seu peso:"));
-var alturaUsuario = parseFloat(validador(alturaUsuario, "Informe a sua altura:"));
+// var nomeUsuario = validador(nomeUsuario, "Informe o seu nome:");
+// var pesoUsuario = parseFloat(validador(pesoUsuario, "Informe o seu peso:"));
+// var alturaUsuario = parseFloat(validador(alturaUsuario, "Informe a sua altura:"));
 
-if (isNaN(pesoUsuario) || isNaN(alturaUsuario)) {
-    document.writeln("Valores inválidos inseridos!")
-} else {
-    var imcUsuario = calcularIMC(pesoUsuario, alturaUsuario);
-    document.writeln(`IMC ${nomeUsuario} = ${imcUsuario}`);
-}
+// if (isNaN(pesoUsuario) || isNaN(alturaUsuario)) {
+//     document.writeln("Valores inválidos inseridos!")
+// } else {
+//     var imcUsuario = calcularIMC(pesoUsuario, alturaUsuario);
+//     document.writeln(`IMC ${nomeUsuario} = ${imcUsuario}<br>`);
 
-function calcularIMC(peso, altura) {
-    let imc = peso / (altura * altura);
-    return imc;
-}
+//     if (imcUsuario < 18.5) {
+//         document.writeln("IMC abaixo do recomendado!");
+//     } else if (imcUsuario >= 18.5 && imcUsuario <= 35) {
+//         document.writeln("IMC normal!");
+//     } else {
+//         document.writeln("IMC acima do recomendado!");
+//     }
+// }
 
-function validador(variavel, mensagemPrompt) {
-    while (variavel == "" || variavel == null) {
-        variavel = prompt(mensagemPrompt);
+// function calcularIMC(peso, altura) {
+//     let imc = peso / (altura * altura);
+//     return imc;
+// }
+
+// function validador(variavel, mensagemPrompt) {
+//     while (variavel == "" || variavel == null) {
+//         variavel = prompt(mensagemPrompt);
+//     }
+
+//     return variavel;
+// }
+
+var anosBrasilCampeao = [1958, 1962, 1970, 1994, 2002];
+var anoCorrente = new Date().getFullYear();
+var anoInicioCopas = 1930;
+// while (anoInicioCopas <= anoCorrente) {
+//     document.writeln(`<br>Tivemos Copa do Mundo no ano de ${anoInicioCopas}<br>`);
+
+//     if (anosBrasilCampeao.includes(anoInicioCopas)) {
+//         document.writeln("Brasil campeão!!<br>");
+//     }
+//     anoInicioCopas += 4;
+// }
+
+for (let index = anoInicioCopas; index <= anoCorrente; index += 4) {
+    document.writeln(`<br>Tivemos Copa do Mundo no ano de ${index}<br>`);
+
+    if (anosBrasilCampeao.includes(index)) {
+        document.writeln("Brasil campeão!!<br>");
     }
-
-    return variavel;
 }
